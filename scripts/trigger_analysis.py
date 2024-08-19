a4f8739bcc59433fa7d0a94792b71e9efb46e86c
zyy <1076192792@QQ.com>
2024-08-19 12:31:03 -0400

    new table format for trigger test

--- scripts/trigger_analysis.py
+++ scripts/trigger_analysis.py
@@ -4,21 +4,31 @@ def trigger_analysis(build_path, target_core, prefix_list):
     trigger_statistic = {}
 
     for prefix in prefix_list:
+        # os.system(f'make analysis PREFIX={prefix} TARGET_CORE={target_core}')
         repo_path = os.path.join(build_path, f'{target_core}_{prefix}.template_repo', 'trigger_analysis_result.md')
         with open(repo_path) as file:
             lines = file.readlines()[2::2]
         for line in lines:
-            trigger_type, train_type, summary, success, rate = line.strip().strip('|').split('|')
+            trigger_type, train_type, summary, success, rate, line_num, valid_num, case_num = line.strip().strip('|').split('|')
             summary = int(summary)
             success = int(success)
             rate = float(rate)
+            line_num = int(line_num)
+            valid_num = int(valid_num)
+            case_num = int(case_num)
             if trigger_type in trigger_statistic:
                 trigger_statistic[trigger_type]['summary'] += summary
                 trigger_statistic[trigger_type]['success'] += success
+                trigger_statistic[trigger_type]['line_num'] += line_num
+                trigger_statistic[trigger_type]['valid_num'] += valid_num
+                trigger_statistic[trigger_type]['case_num'] += case_num
             else:
                 trigger_statistic[trigger_type] = {
                     'summary': summary,
                     'success': success,
+                    'line_num': line_num,
+                    'valid_num': valid_num,
+                    'case_num': case_num,
                 }
 
     table_entry = [
@@ -38,8 +48,13 @@ def trigger_analysis(build_path, target_core, prefix_list):
         table[head] = {
             'success':0,
             'summary':0,
+            'line_num':0,
+            'valid_num':0,
+            'case_num':0,
             'success_rate':0,
-            'summary_rate':0
+            'summary_rate':0,
+            'ave_line_num':0,
+            'ave_valid_num':0
         }
 
     for trigger_type, content in trigger_statistic.items():
@@ -65,6 +80,9 @@ def trigger_analysis(build_path, target_core, prefix_list):
                 raise Exception(f'{trigger_type} can not be statisted')
         table[entry]['success'] += content['success']
         table[entry]['summary'] += content['summary']
+        table[entry]['line_num'] += content['line_num']
+        table[entry]['valid_num'] += content['valid_num']
+        table[entry]['case_num'] += content['case_num']
         summary += content['success']
 
     for entry in table.keys():
@@ -76,24 +94,15 @@ def trigger_analysis(build_path, target_core, prefix_list):
             table[entry]['summary_rate'] = table[entry]['success']/summary
         except ZeroDivisionError:
             table[entry]['summary_rate'] = 0
+        try:
+            table[entry]['ave_line_num'] = table[entry]['line_num']/table[entry]['case_num']
+        except ZeroDivisionError:
+            table[entry]['ave_line_num'] = 0
+        try:
+            table[entry]['ave_valid_num'] = table[entry]['valid_num']/table[entry]['case_num']
+        except ZeroDivisionError:
+            table[entry]['ave_valid_num'] = 0
     
-    sum_line = 0
-    sum_valid = 0
-    sum_case = 0
-    for prefix in prefix_list:
-        repo_path = os.path.join(build_path, f'{target_core}_{prefix}.template_repo', 'reduce_analysis_result.md')
-        with open(repo_path, "rt") as file:
-            lines = file.readlines()
-            line_num = float(lines[0].strip().split()[-1])
-            valid_num = float(lines[1].strip().split()[-1])
-        repo_path = os.path.join(build_path, f'{target_core}_{prefix}.template_repo', 'trigger_template')
-        case_num = len(os.listdir(repo_path))
-        sum_line += case_num * line_num
-        sum_valid += case_num * valid_num
-        sum_case += case_num
-        print(case_num, line_num, valid_num)
-    print()
-    table['overhead'] = {'line_num':sum_line/sum_case, 'valid_num':sum_valid/sum_case}
     return table
 
 final_table = {}
@@ -102,7 +111,10 @@ build_path = 'build'
 target_core = 'BOOM'
 prefix_list = [
     'trigger_test_00',
+    'trigger_test_01',
+    'trigger_test_02',
     'trigger_test_03',
+    'trigger_test_04',
 ]
 final_table['BOOM-I'] = trigger_analysis(build_path, target_core, prefix_list)
 
@@ -124,6 +136,7 @@ final_table['BOOM-III'] = trigger_analysis(build_path, target_core, prefix_list)
 
 target_core = 'XiangShan'
 prefix_list = [
+    'trigger_test_00',
     'trigger_test_01',
     'trigger_test_02',
     'trigger_test_03',
@@ -132,12 +145,14 @@ final_table['XiangShan-I'] = trigger_analysis(build_path, target_core, prefix_li
 
 prefix_list = [
     'unalign_trigger_test_00',
+    'unalign_trigger_test_01',
     'unalign_trigger_test_02',
     'unalign_trigger_test_03',
 ]
 final_table['XiangShan-II'] = trigger_analysis(build_path, target_core, prefix_list)
 
 prefix_list = [
+    'random_trigger_test_00',
     'random_trigger_test_01',
     'random_trigger_test_02',
     'random_trigger_test_03',
@@ -167,7 +182,15 @@ with open('trigger_statstic.md', 'wt') as file:
 
     for trigger_type, sub_table in final_table.items():
         file.write(f'|{trigger_type}|')
+        line_num_sum = 0
+        valid_num_sum = 0
+        case_num = 0
         for entry in table_entry:
-            file.write(f"{round(sub_table[entry]['summary_rate']*100, 2)}%({round(sub_table[entry]['success_rate']*100, 2)}%)|")
-        file.write(f"{round(sub_table['overhead']['line_num'], 2)}({round(sub_table['overhead']['valid_num'], 2)})|\n")
+            line_num = round(sub_table[entry]['ave_line_num'], 2)
+            valid_num = round(sub_table[entry]['ave_valid_num'], 2)
+            line_num_sum += line_num
+            valid_num_sum += valid_num
+            case_num += 1
+            file.write(f"{round(sub_table[entry]['success_rate']*100, 2)}%/{line_num}({valid_num})|")
+        file.write(f"{round(line_num_sum/case_num,2)}({round(valid_num_sum/case_num,2)})|\n")
         
