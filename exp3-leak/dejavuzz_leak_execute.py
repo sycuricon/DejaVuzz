3feef89dabc92c20867f350409cfb1750140c0f6
zhouyangye1076 <1076192792@QQ.com>
2024-12-20 08:36:38 +0800

    fix leak time out examine bug

--- exp3-leak/dejavuzz_leak_execute.py
+++ exp3-leak/dejavuzz_leak_execute.py
@@ -10,23 +10,37 @@ LEAK_MAX_TIME = 30 * 60
 EXAMINE_INTERVAL = 1 * 60
 TARGET_LEAK_NUM = 20000
 
+start_time = time.time()
+
+def record_log(filename, log_string):
+    end_time = time.time()
+    with open(filename, "a+") as file:
+        file.write(f'{end_time - start_time}\t{log_string}\n')
+
 def execute_command(stop_event:threading.Event, command:str, sleep_interval):
-    print(command)
+    # print(command)
     process = subprocess.Popen(command, shell=True, preexec_fn=os.setpgrp)
     while not stop_event.is_set():
         if process.poll() is not None:
             break
         time.sleep(sleep_interval)
     os.killpg(process.pid, signal.SIGTERM)
-    print("process terminal")
+    # print("process terminal")
     time.sleep(1)
 
 def dejavuzz_execute_and_analysis(repo_prefix, group_prefix):
+    def dejavuzz_record_log(log_string):
+        record_log(log_file, log_string)
+
     current_path = os.path.abspath(__file__)
     current_folder = os.path.dirname(current_path)
     repo_path = os.path.join(current_folder, repo_prefix)
+    log_file = os.path.join(repo_path, 'fuzz.log')
     if not os.path.exists(repo_path):
         os.mkdir(repo_path)
+    if not os.path.exists(log_file):
+        dejavuzz_record_log(f'create {log_file}')
+    dejavuzz_record_log(f'fuzz {group_prefix}')
 
     fuzz_path = os.path.join(current_folder, '..', 'build', f'BOOM_{group_prefix}')
     assert not os.path.exists(fuzz_path), f"the repo {fuzz_path} has existed, please delete that repo or execute script again"
@@ -40,26 +54,30 @@ def dejavuzz_execute_and_analysis(repo_prefix, group_prefix):
     run_time_summary = 0
     leak_time_counter = 0
     leak_iter_num = 0
+    last_leak_iter_num = 0
     execute_result = False
     while fuzz_thread.is_alive():
         time.sleep(EXAMINE_INTERVAL)
         run_time_summary += EXAMINE_INTERVAL
         leak_time_counter += EXAMINE_INTERVAL
 
-        old_leak_iter_num = leak_iter_num
         if os.path.exists(leak_iter_num_path):
             with open(leak_iter_num_path, 'rt') as file:
                 leak_iter_num = int(file.read())
-        new_leak_iter_num = leak_iter_num - old_leak_iter_num
 
         if leak_time_counter >= LEAK_MAX_TIME:
+            new_leak_iter_num = leak_iter_num - last_leak_iter_num
+            dejavuzz_record_log(f'leak_time:{leak_time_counter}\tleak_num:{leak_iter_num}\tlast_leak_num:{last_leak_iter_num}\tnew_leak_num:{new_leak_iter_num}')
             if new_leak_iter_num == 0:
-                print("leak fuzz time out")
-                break
+                dejavuzz_record_log(f'fuzz {group_prefix} fails, because new leak time out')
+                # print("leak fuzz time out")
+                break 
             leak_time_counter %= LEAK_MAX_TIME
-
+            last_leak_iter_num = leak_iter_num
+        
         if leak_iter_num >= TARGET_LEAK_NUM - 1:
-            print("fuzz success!!!")
+            dejavuzz_record_log(f"fuzz {group_prefix} success")
+            # print("fuzz success!!!")
             execute_result = True
             break
 
@@ -73,12 +91,13 @@ def dejavuzz_execute_and_analysis(repo_prefix, group_prefix):
         to_path = os.path.join(repo_path, group_prefix)
         command = f'cp {from_path} {to_path}'
         assert not os.system(command), f"fails to copy {from_path}"
+        dejavuzz_record_log(f'store coverage of {group_prefix}')
 
     return execute_result
 
 if __name__ == "__main__":
     repo_prefix = f'dejavuzz_result_{hex(random.randint(0, 2**32-1))}'
-    print(f"the dejavuzz coverage info is stored in {repo_prefix}")
+    # print(f"the dejavuzz coverage info is stored in {repo_prefix}")
 
     iter_num = 0
     while iter_num < ITER_NUM:
@@ -87,9 +106,9 @@ if __name__ == "__main__":
         result = dejavuzz_execute_and_analysis(repo_prefix, group_prefix)
         end = time.time()
         time_interval = end - start
-        print("use time:", time_interval/3600.0, "h")
+        # print("use time:", time_interval/3600.0, "h")
         if result:
-            print("dejavuzz executes successfully")
+            # print("dejavuzz executes successfully")
             iter_num += 1
-        else:
-            print("dejavuzz executed failed")
+        # else:
+        #     print("dejavuzz executed failed")
