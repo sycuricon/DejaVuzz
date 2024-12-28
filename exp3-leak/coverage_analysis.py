40224e0fb9be4d326775bb1407f26ee24be01d8e
zhouyangye1076 <1076192792@QQ.com>
2024-12-28 10:09:34 +0800

    choose result folder to draw plot by args

--- exp3-leak/coverage_analysis.py
+++ exp3-leak/coverage_analysis.py
@@ -1,8 +1,9 @@
+import datetime
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 import numpy as np
 import os
-import random
+import argparse
 import scipy.stats as stats
 
 CURVE_LEN = 20000
@@ -70,41 +71,6 @@ def spec_get_curve(spec_cov_list):
         curve.append(new_num)
     return curve
 
-def smooth(curve, thres):
-    last_protrude = 0
-    new_protrude = 0
-    concavity = 0
-
-    i = 1
-    while i < len(curve) - 1:
-        last_grad = curve[i] - curve[i-1]
-        new_grad = curve[i+1] - curve[i]
-        if last_grad > thres and new_grad ==0 :
-            last_protrude = i
-            i += 1
-            break
-        else:
-            i += 1
-
-    while i < len(curve) - 1:
-        while i < len(curve) - 1:
-            last_grad = curve[i] - curve[i-1]
-            new_grad = curve[i+1] - curve[i]
-            if last_grad > thres and new_grad ==0 :
-                new_protrude = i
-                i += 1
-                break
-            else:
-                i += 1
-
-        if new_protrude > last_protrude:
-            for i in range(last_protrude + 1, new_protrude):
-                curve[i] = (curve[new_protrude] * (i - last_protrude) + curve[last_protrude] * (new_protrude - i))
-                curve[i] /= new_protrude - last_protrude
-            last_protrude = new_protrude
-    
-    return curve
-
 def dejavuzz_get_curve(filename):
     with open(filename) as file:
         lines = file.readlines()
@@ -114,48 +80,30 @@ def dejavuzz_get_curve(filename):
         coverage = int(line[-1])
         curve.append(coverage)
 
-    curve = smooth(curve, 200)
-    curve = smooth(curve, 100)
-    curve = smooth(curve, 50)
-
     return curve
 
-if __name__ == "__main__":
-    current_file_path = os.path.abspath(__file__)
-    current_folder_path = os.path.dirname(current_file_path)
-    dejavuzz_result_folder_list = []
-    specdoctor_result_folder_list = []
-    for folder in os.listdir(current_folder_path):
-        if folder.startswith('dejavuzz_result'):
-            dejavuzz_result_folder_list.append(os.path.join(current_folder_path, folder))
-            continue
-        if folder.startswith('specdoctor_result'):
-            specdoctor_result_folder_list.append(os.path.join(current_folder_path, folder))
-            continue
+def analysis_and_draw(specdoctor_path, dejavuzz_path):
+    assert os.path.exists(specdoctor_path), f'{specdoctor_path} doesn\'t exist'
+    assert os.path.exists(dejavuzz_path), f'{dejavuzz_path} doesn\'t exist'
 
     dejavuzz_curve = []
-    for folder in dejavuzz_result_folder_list:
-        for dejavuzz_curve_file in os.listdir(folder):
-            dejavuzz_curve_file_path = os.path.join(folder, dejavuzz_curve_file)
-            dejavuzz_curve.append(dejavuzz_get_curve(dejavuzz_curve_file_path))
+    for dejavuzz_curve_file in os.listdir(dejavuzz_path):
+        if dejavuzz_curve_file == 'fuzz.log':
+            continue
+        dejavuzz_curve_file_path = os.path.join(dejavuzz_path, dejavuzz_curve_file)
+        dejavuzz_curve.append(dejavuzz_get_curve(dejavuzz_curve_file_path))
     dejavuzz_curve_len = min(min([len(curve) for curve in dejavuzz_curve]), CURVE_LEN)
     dejavuzz_curve = [curve[:dejavuzz_curve_len] for curve in dejavuzz_curve]
 
     specdoctor_curve = []
-    for folder in specdoctor_result_folder_list:
-        legend = [
-            'U2M_ATTACKER', 
-            'S2M_ATTACKER',
-            'U2S_ATTACKER',
-            'U2S_VICTIM',
-            'S2M_VICTIM',
-        ]
-        cov_dir = [os.path.join(folder, sub_legend) for sub_legend in legend]
-        specdoctor_curve_len = CURVE_LEN
-        file_in_dir_list = [get_spec_cov_list(filename)[:specdoctor_curve_len] for filename in cov_dir]
-        curve_list = [spec_get_curve(file_in_dir)[:specdoctor_curve_len] for file_in_dir in file_in_dir_list]
-        specdoctor_curve.extend(curve_list)
-        break
+    for specdoctor_cov_folder in os.listdir(specdoctor_path):
+        if os.path.isfile(specdoctor_cov_folder):
+            continue
+        file_in_dir = get_spec_cov_list(os.path.join(specdoctor_path, specdoctor_cov_folder))
+        curve = spec_get_curve(file_in_dir)
+        specdoctor_curve.append(curve)
+    specdoctor_curve_len = min(min([len(curve) for curve in specdoctor_curve]), CURVE_LEN)
+    specdoctor_curve = [curve[:specdoctor_curve_len] for curve in specdoctor_curve]
 
     draw_plot(dejavuzz_curve, label='DejaVuzz', color="#2878b5")
     draw_plot(specdoctor_curve, label='SpecDoctor', color="#ffbe7a")
@@ -170,8 +118,22 @@ if __name__ == "__main__":
     plt.gca().yaxis.set_major_formatter(FuncFormatter(lambda x, pos : f"{int(x / 1000)}k"))
 
     plt.tight_layout()
-    random_prefix = hex(random.randint(0, 2**32-1))
-    coverage_name = f'Exp3_Coverage_{random_prefix}.pdf'
+
+    current_time = datetime.datetime.now()
+    time_str = current_time.strftime("%Y-%m-%d-%H-%M-%S")
+    coverage_name = f'Exp3_Coverage_{time_str}.pdf'
     plt.savefig(coverage_name, bbox_inches = 'tight', pad_inches = 0)
     print(f'store coverage figure in {coverage_name}')
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dejavuzz", dest="dejavuzz_path", required=True, help="the path of dejavuzz result")
+    parser.add_argument("--specdoctor", dest="specdoctor_path", required=True, help="the path of secdoctor result")
+    args = parser.parse_args()
+
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    dejavuzz_folder = os.path.basename(args.dejavuzz_path)
+    specdoctor_folder = os.path.basename(args.specdoctor_path)
+    dejavuzz_path = os.path.join(current_path, dejavuzz_folder)
+    specdoctor_path = os.path.join(current_path, specdoctor_folder)
+    analysis_and_draw(specdoctor_path, dejavuzz_path)
