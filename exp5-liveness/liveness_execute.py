19303c6c6e1c827dd25f9cd74c40cd5c78ef0aac
zyy <1076192792@QQ.com>
2025-02-13 04:30:55 -0500

    add liveness script

---
+++ exp5-liveness/liveness_execute.py
@@ -0,0 +1,68 @@
+import os
+import time
+import datetime
+import threading
+
+THREAD_NUM = 64
+
+def system_call(string):
+    print(string)
+    for i in range(3):
+        if os.system(string) != 0:
+            time.sleep(10)
+        else:
+            return
+    else:
+        raise Exception('cannot handle this system call by delay')
+
+def case_execute(input_directory, output_directory, wave_directory, index, target_core):
+    input_path = os.path.join(input_directory, str(index))
+    swap_cfg_path = os.path.join(input_path, 'spec.cfg')
+    live_name = os.path.join(wave_directory, f'spec_{index%THREAD_NUM}.taint.live')
+    output_path = os.path.join(output_directory, f'{index}.taint.live')
+    system_call(f'make -C {target_core} vcs STARSHIP_TESTCASE={swap_cfg_path} SIMULATION_LABEL=spec_{index%THREAD_NUM} SIMULATION_MODE=variant')
+    system_call(f'cp {live_name} {output_path}')
+
+def casedataset_execute(repo_prefix):
+    current_file = os.path.abspath(__file__)
+    current_folder = os.path.dirname(current_file)
+    case_dataset_path = os.path.join(current_folder, 'liveness_dataset')
+
+    repo_path = os.path.join(current_folder, repo_prefix)
+    if not os.path.exists(repo_path):
+        os.mkdir(repo_path)
+
+    for target_core in ['BOOM', 'BOOM_without_mask']:
+        result_output_path = os.path.join(repo_path, target_core)
+        if not os.path.exists(result_output_path):
+            os.mkdir(result_output_path)
+
+        wave_path = os.path.join(current_folder, target_core,\
+            'build', 'vcs', 'BOOM_variant', 'wave')
+
+        index_sum = len(os.listdir(case_dataset_path))
+        index = 0
+        while index < index_sum:
+            index_list = []
+            for i in range(THREAD_NUM):
+                if index >= index_sum:
+                    break
+                index_list.append(index)
+                index += 1
+
+            thread_list = []
+            for file_index in index_list:
+                thread = threading.Thread(target=case_execute, \
+                    args=(case_dataset_path, result_output_path, wave_path, file_index, 'BOOM'))
+                thread.start()
+                thread_list.append(thread)
+            for thread in thread_list:
+                thread.join()
+
+
+if __name__ == "__main__":
+    current_time = datetime.datetime.now()
+    time_str = current_time.strftime("%Y-%m-%d-%H-%M-%S")
+    repo_prefix = f'liveness_result_{time_str}'
+    casedataset_execute(repo_prefix)
+
