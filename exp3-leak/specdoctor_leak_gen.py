b7190e6498e83a5078bb7ef4a7039e8eeb26da9c
zyy <1076192792@QQ.com>
2025-02-28 12:20:12 -0500

    add riscv elf in dataset gen in exp3

--- exp3-leak/specdoctor_leak_gen.py
+++ exp3-leak/specdoctor_leak_gen.py
@@ -9,31 +9,46 @@ def system_call(string):
     if os.system(string):
         exit()
 
-def spec_fuzz(dirname:str):
+def spec_fuzz(dirname:str, i):
     raw_dir_list = os.listdir(dirname)
     dir_list = []
     
     for file in raw_dir_list:
         if file.endswith('s0.riscv') and file.startswith('.t3'):
             dir_list.append(os.path.join(dirname, file))
+    def get_index(file_path):
+        token = file_path.split('_')
+        return int(token[-2])
+    dir_list.sort(key=get_index, reverse=False)
 
     current_file = os.path.abspath(__file__)
     current_folder = os.path.dirname(current_file)
-    dataset_path = os.path.join(current_folder, 'leak_dataset', 'specdoctor', dirname.split('/')[-1])
+    dataset_path = os.path.join(current_folder, 'leak_dataset', 'specdoctor', f'spec_{i}')
     if not os.path.exists(dataset_path):
         os.mkdir(dataset_path)
 
-    for index,file_name in enumerate(dir_list):
+    index = 0
+    for file_name in dir_list:
+        dut_file_name = file_name
+        vnt_file_name = file_name.replace('s0', 's1')
+        if not os.path.exists(vnt_file_name):
+            with open('.log', 'a+') as file:
+                file.write(f'{vnt_file_name} is not exists\n')
+            continue
+
         case_path = os.path.join(dataset_path, str(index))
         if not os.path.exists(case_path):
             os.mkdir(case_path)
 
-        dut_file_name = file_name
         dut_file_bin = os.path.join(case_path, f'dut.bin')
-        vnt_file_name = file_name.replace('s0', 's1')
+        dut_file_elf = os.path.join(case_path, f'dut.elf')
         vnt_file_bin = os.path.join(case_path, f'vnt.bin')
+        vnt_file_elf = os.path.join(case_path, f'vnt.elf')
+        
         system_call(f'riscv64-unknown-elf-objcopy -O binary {dut_file_name} {dut_file_bin}')
         system_call(f'riscv64-unknown-elf-objcopy -O binary {vnt_file_name} {vnt_file_bin}')
+        system_call(f'cp {dut_file_name} {dut_file_elf}')
+        system_call(f'cp {vnt_file_name} {vnt_file_elf}')
 
         variant_template = {
             'start_addr': libconf.LibconfInt64(0x8000_0000),
@@ -57,20 +72,20 @@ def spec_fuzz(dirname:str):
         config_name = os.path.join(case_path, f'spec.cfg')
         with open(config_name, 'wt') as file:
             libconf.dump(variant_template, file)
+        
+        index += 1
 
 if __name__ == "__main__":
     directory = [
-        # "/eda/specdoc-eval/S2M_ATTACKER",
+        "/eda/specdoc-eval/S2M_ATTACKER",
         "/eda/specdoc-eval/S2M_VICTIM",
-        # "/eda/specdoc-eval/S2M_VICTIM_2",
         "/eda/specdoc-eval/U2M_ATTACKER",
         "/eda/specdoc-eval/U2S_ATTACKER",
         "/eda/specdoc-eval/U2S_VICTIM",
-        # "/eda/specdoc-eval/U2S_VICTIM_2",
     ]
 
-    for dir_name in directory:
-        spec_fuzz(dir_name)
+    for i, dir_name in enumerate(directory):
+        spec_fuzz(dir_name, i)
     
 
 
